114f0c48d53e798f584534584dccececf6dd5759
Jannis R <mail@jannisr.de>
2017-08-30 02:10:37 +0200

    WIP part 2

--- index.js
+++ index.js
@@ -2,10 +2,8 @@
 
 const paths = require('env-paths')('download-chakracore')
 const path = require('path')
-const mkdirp = require('mkdirp')
-const fs = require('fs')
-const fetch = require('node-fetch')
 
+const {ensureDir, fileExists, downloadToPath} = require('./lib/helpers')
 const findAsset = require('./lib/find-asset')
 
 const download = (version, arch, platform, cache, cb) => {
@@ -13,53 +11,23 @@ const download = (version, arch, platform, cache, cb) => {
 	if (version !== 'latest' && version[0] !== 'v') version = 'v' + version
 	arch = arch || process.arch
 	platform = platform || process.platform
-	cache = cache || process.platform
+	cache = cache || paths.cache
 	console.error('version', version, 'arch', arch, 'platform', platform, 'cache', cache)
 
-	mkdirp(cache, (err) => {
-		if (err) return cb(err)
+	ensureDir(cache)
+	.then(() => findAsset(version, arch, platform, cache))
+	.then((asset) => {
+		const dest = path.join(cache, asset.name)
 
-		const file = ['node', version, platform, arch].join('-') + '.zip'
-		const dest = path.join(cache, file)
-		console.error('dest', dest)
-		fs.stat(dest, (err, stats) => {
-			if (err && err.code !== 'ENOENT') return cb(err)
-
-			// todo: what if it exists?
-			// const exists = !err
-			// if (exists) return cb(dest)
-
-			findAsset(version, arch, platform, cache)
-			.then((asset) => {
-				console.error('asset', asset)
-			})
-			// const src = baseUrl + version + '/' + file
-			// console.error('src', src)
-			// fetch(src, {redirect: 'follow'})
-			// .then((res) => {
-			// 	if (!res.ok) {
-			// 		const err = new Error(res.statusText)
-			// 		err.statusCode = res.status
-			// 		return cb(err)
-			// 	}
-
-			// 	const sink = fs.createWriteStream(dest)
-
-			// 	res.body.once('error', (err) => {
-			// 		res.body.destroy()
-			// 		cb(err)
-			// 	})
-			// 	sink.once('error', (err) => {
-			// 		sink.destroy()
-			// 		cb(err)
-			// 	})
-
-			// 	res.body.pipe(sink)
-			// 	sink.once('finish', cb)
-			// })
-			// .catch(cb)
+		return fileExists(dest)
+		.then((exists) => {
+			if (!exists) return downloadToPath(asset.url, dest)
+		})
+		.then(() => {
+			console.log('done!', dest)
 		})
 	})
+	.catch(cb)
 }
 
 module.exports = download
